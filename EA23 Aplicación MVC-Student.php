151b2cc6a303734c52c396d53488121e4efc0c1f
Laylah611 <118370468+Laylah611@users.noreply.github.com>
2022-11-17 08:55:21 -0600

    Create EA23 Aplicación MVC-Student.php

---
+++ EA23 Aplicación MVC-Student.php
@@ -0,0 +1,35 @@
+<!--Archivo de vista, este archivo en su interior solo recibe como parametro un arreglo para que este pueda ser iterado y mostrado a manera de tabla en PHP, con sus respectivas acciones para crear uno nuevo, editar y elimiar.-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<title>Students</title>
+</head>
+<body>
+	<style>
+		.Tabla{
+			
+		}
+	</style>
+
+
+
+	<h1><center>Registro de estudiantes</center></h1>
+	<div class="Tabla">
+		<table id="table">
+			<tr>
+			<td>Nombre</td>
+			<td>Primer Apellido</td>
+			<td>Segundo Apellido</td>
+			<td>Matrícula</td>
+			</tr>
+		</table>
+	</div>
+	
+	
+
+</body>
+</html>
